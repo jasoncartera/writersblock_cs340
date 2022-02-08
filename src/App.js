34c845cf079b6de482ce56cf68f83b6933b8f2bd
Ravonne Byrd <rnevels10@gmail.com>
2022-02-08 16:47:00 -0500

    test workflow; app.js modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Link } from "react-router-dom";
-import Writers from './pages/Writers';
 
 function App() {
   return (
@@ -9,7 +8,7 @@ function App() {
         <h1>Welcome to WritersBlock Management Page</h1>
         <Link to="/Writers">Manage Writers</Link>
         <Link to="/Readers">Manage Readers</Link>
-        <Link to="/WritersReaders">Manger WritersReaders</Link>
+        <Link to="/WritersReaders">Manage WritersReaders</Link>
         <Link to="/Posts">Manage Posts</Link>
         <Link to="/Comments">Manage Comments</Link>
       </div>
