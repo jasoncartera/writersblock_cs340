b090d400542934ef8790fc08e090c090800e7aed
Ravonne Byrd <rnevels10@gmail.com>
2022-02-08 22:01:02 -0500

    modified repo to have both server and ui project

---
+++ server/db/connection.mjs
@@ -0,0 +1,53 @@
+'use strict';
+
+import { createConnection } from 'mysql';
+
+const db = createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+/* 
+Citation for the following connect function's use of create table:
+Date: 2/8/2022
+Adapted from: Nodes.js MySQL Create Table on w3school
+Source URL: https://www.w3schools.com/nodejs/nodejs_mysql_create_table.asp
+*/
+ 
+/* Connect to database and create necessary tables if they do not exist. */
+db.connect(function(error) {
+  if (error) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  /* Display connection id. */
+  console.log('db id: ' + connection.threadId);
+
+  /* Creating the Writers table. */
+  const writersTableCreate = `create table if not exists Writers(
+    Id: INT auto_increment NOT NULL primary key,
+    Username: NVARCHAR(15) unique NOT NULL,
+    Email: NVARCHAR(255) unique NOT NULL,
+    Photo: NVARCHAR(max) NULL,
+    DateJoined: DATETIME NOT NULL
+  )`;
+
+  db.query(writersTableCreate, function (error, results, fields) {
+    if (error){
+      console.log("Cannot create Writers table.");
+      throw error;
+    } else {
+      console.log("Writers table created");
+    };
+  });
+ 
+});
+
+export { db }
+
+
+// close connection?
+// db_connect.end();
---
+++ server/db/writers_methods.mjs
@@ -0,0 +1,17 @@
+import { db } from './connection.mjs'
+
+/* 
+    Writers table methods.
+*/
+
+/* Create a new writer. */
+
+/* Retrieve a writer. */
+
+/* Retrieve all writer. */
+
+/* Update a writer. */
+
+/* Update a writer. */
+
+/* Delete a writer */
