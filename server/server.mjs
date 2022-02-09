fd4a64f7162bbf319d15258dd634a0ba023c3dc2
Ravonne Byrd <rnevels10@gmail.com>
2022-02-08 23:35:33 -0500

    modified server port to be compatible with heroku's port

--- server/server.mjs
+++ server/server.mjs
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
