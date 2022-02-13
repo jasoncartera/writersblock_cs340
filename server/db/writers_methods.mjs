0d9a62fed8cb469c9f27ba349a6e3b5cc166a11b
Jason Carter <carterjasona@gmail.com>
2022-02-13 14:26:55 -0800

    set up get all writers

--- server/db/writers_methods.mjs
+++ server/db/writers_methods.mjs
@@ -1,17 +1,41 @@
-import { db } from './connection.mjs'
-
+import { dbQuery } from './connection.mjs'
+import express from 'express';
+const router = express.Router();
 /* 
     Writers table methods.
 */
+
 
 /* Create a new writer. */
 
 /* Retrieve a writer. */
 
 /* Retrieve all writer. */
+const selectAllWrtiers = async () => {
+    const selectAllWriters = `SELECT * FROM Writers`
+    const result = await dbQuery(selectAllWriters);
+    return result;
+}
 
 /* Update a writer. */
 
 /* Update a writer. */
 
 /* Delete a writer */
+
+/* Routes */
+
+/* Adapted from 'Example of Writing GET route using async/await promises:
+https://darifnemma.medium.com/how-to-interact-with-mysql-database-using-async-await-promises-in-node-js-9e6c81b683da  */
+router.get('/writers', async (req, res) => {
+    try {
+        const data = await selectAllWrtiers();
+        // Send json response
+        res.status(200).json({ writers: data });
+      } catch (err) {
+        console.log(err);
+        res.status(500);
+      }
+});
+
+export default router;
--- server/server.mjs
+++ server/server.mjs
@@ -1,13 +1,22 @@
 'use strict';
 
 import express from 'express';
+import writers  from './db/writers_methods.mjs'
+//import { selectAllWrtiers } from './db/writers_methods.mjs';
 
 const app = express();
 const PORT = 3000;
 
+
+app.use(writers);
+
+
 app.get("/*", (req, res) => {
   res.send("Hello World!");
 });
+
+
+
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
