39b4d85bd30e769d697f42fb0b07d27012d2611e
Jason Carter <carterjasona@gmail.com>
2022-02-28 13:52:16 -0800

    sort tables

--- ui/src/components/writersReaders/WriterReaderList.js
+++ ui/src/components/writersReaders/WriterReaderList.js
@@ -10,6 +10,8 @@ How table is broken up into components is Adapted from:
 Source URL: https://canvas.oregonstate.edu/courses/1830200/pages/exploration-implementing-a-full-stack-mern-app-part-1?module_item_id=21362841
 */
 function WriterReaderList({ writersReaders }) {
+    writersReaders.sort((a, b) => a.Id > b.Id ? 1: -1);
+
     return (
         <>
             <p>Current WritersReaders</p>
